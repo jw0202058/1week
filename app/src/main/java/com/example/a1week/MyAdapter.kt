881a267a98afc9650b64e3d09df229d555eb28de
jw0202058 <jw0202058@korea.ac.kr>
2023-06-30 11:58:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a1week/MyAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.a1week
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+@Suppress("DEPRECATION")
+internal class MyAdapter(
+    var context: Context,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                Call()
+            }
+            1 -> {
+                Gallery()
+            }
+            2 -> {
+                Tap3()
+            }
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
