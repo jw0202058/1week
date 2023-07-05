da28bc0cfcf5770f1a9e448c97cda2ff0fa46caa
jw0202058 <jw0202058@korea.ac.kr>
2023-07-05 13:52:27 +0900

    갤러리 사진 불러오기

--- app/src/main/java/com/example/a1week/Gallery.kt
+++ app/src/main/java/com/example/a1week/Gallery.kt
@@ -2,12 +2,16 @@ package com.example.a1week
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,33 +19,112 @@ import android.widget.BaseAdapter
 import android.widget.GridView
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Gallery : Fragment() {
+    private val REQUEST_CODE_PERMISSION = 123
+    private val REQUEST_CODE_PICK_IMAGE = 456
+
+    private lateinit var gv: GridView
+    private lateinit var gAdapter: MyGridAdapter
+
+    private lateinit var fabMain: FloatingActionButton
 
     @SuppressLint("MissingInflatedId")
-    override fun onCreateView(              // 생성된 뷰를 반환하여 화면에 보여 줌
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val gv = view.findViewById<GridView>(R.id.gridView)
-        val gAdapter = MyGridAdapter(requireContext())
+        gv = view.findViewById<GridView>(R.id.gridView)
+        gAdapter = MyGridAdapter(requireContext())
         gv.adapter = gAdapter
+
+        fabMain = view.findViewById<FloatingActionButton>(R.id.fabMain)
+        fabMain.setOnClickListener {
+            if (checkPermission()) {
+                openGallery()
+            } else {
+                requestPermission()
+            }
+        }
 
         return view
     }
 
-        inner class MyGridAdapter(private val context: Context) : BaseAdapter() {
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
 
-        private val picID = arrayListOf<Int>(           // 사진 받아오기
-            R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1,
-            R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1,
-            )
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_CODE_PERMISSION
+        )
+    }
 
-        private val selectedImages = arrayListOf<Int>()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permission Denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+            if (selectedImageUri != null) {
+                val bitmap = decodeUriToBitmap(requireContext(), selectedImageUri)
+                if (bitmap != null) {
+                    gAdapter.addImage(bitmap)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed to load image",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun decodeUriToBitmap(context: Context, uri: Uri): Bitmap? {
+        return try {
+            val inputStream = context.contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    inner class MyGridAdapter(private val context: Context) : BaseAdapter() {
+
+        private val picID = arrayListOf<Bitmap>()
 
         override fun getCount(): Int {
             return picID.size
@@ -55,26 +138,26 @@ class Gallery : Fragment() {
             return 0
         }
 
-        @SuppressLint("ViewHolder")         // 받아온 사진을 다른 뷰에 할당하여 화면에 띄우기
+        @SuppressLint("ViewHolder")
         override fun getView(i: Int, view: View?, viewGroup: ViewGroup): View {
             val imageView = ImageView(context)
 
             imageView.layoutParams = ViewGroup.LayoutParams(200, 300)
             imageView.scaleType = ImageView.ScaleType.FIT_CENTER
-            imageView.setImageResource(picID[i])
+            imageView.setImageBitmap(picID[i])
 
-            imageView.setOnClickListener {      // imageView 누르면 나타나는 일 (다이얼로그가 뜸)
-                val dialogView = View.inflate(requireContext(), R.layout.dialog, null)          // 뜨는 창
-                val dlg = AlertDialog.Builder(requireContext())             // 길게 쓰기 귀찮으니까 그냥 dlg로 선언해 줌
-                val ivPic = dialogView.findViewById<ImageView>(R.id.ivPic)  // 이미지 받아 오기
-                ivPic.setImageResource(picID[i])            // 다이얼로그의 이미지뷰 id인 ivPic에 위에서 받아온 사진 보여주기
-                dlg.setTitle("큰 이미지")                    //
+            imageView.setOnClickListener {
+                val dialogView = View.inflate(requireContext(), R.layout.dialog, null)
+                val dlg = AlertDialog.Builder(requireContext())
+                val ivPic = dialogView.findViewById<ImageView>(R.id.ivPic)
+                ivPic.setImageBitmap(picID[i])
+                dlg.setTitle("큰 이미지")
                 dlg.setView(dialogView)
                 dlg.setNegativeButton("닫기", null)
                 dlg.show()
             }
 
-            imageView.setOnLongClickListener{
+            imageView.setOnLongClickListener {
                 showImageDialog(i)
                 true
             }
@@ -82,25 +165,28 @@ class Gallery : Fragment() {
             return imageView
         }
 
-            private fun showImageDialog(position: Int) {        // 사진 삭제
+        private fun showImageDialog(position: Int) {
             val dialog = AlertDialog.Builder(requireContext())
                 .setTitle("알림")
                 .setMessage("이 사진을 삭제하시겠습니까?")
                 .setNegativeButton("아니오", null)
-
                 .setPositiveButton("예") { _, _ ->
-                    removeImage(position)               // 밑에 함수에서 구현
+                    removeImage(position)
                 }
                 .create()
 
             dialog.show()
         }
-        private fun removeImage(position: Int) {            // 사진 삭제 후 완료 toast
-            val deletedImageId = picID[position]
-            selectedImages.remove(deletedImageId)
+
+        private fun removeImage(position: Int) {
             picID.removeAt(position)
             notifyDataSetChanged()
             Toast.makeText(requireContext(), "사진이 삭제되었습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        fun addImage(bitmap: Bitmap) {
+            picID.add(0, bitmap)
+            notifyDataSetChanged()
         }
     }
 }
